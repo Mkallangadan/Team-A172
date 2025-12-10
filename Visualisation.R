0683ef54723534d16916d04563c8ee2cf6c4ea55
Keerthi Rakesh <kr24abx@herts.ac.uk>
2025-12-11 02:17:36 +0530

    Visualisation file updated

--- Visualisation.R
+++ Visualisation.R
@@ -4,3 +4,12 @@ library(tidyverse)
 df <- read_csv("vgsales-12-4-2019-short.csv")
 
 print(head(df))
+df_compare <- df %>%
+  mutate(Group = ifelse(Publisher == "Nintendo", "Nintendo", "Others"))
+
+avg_scores <- df_compare %>%
+  group_by(Group) %>% 
+  summarise(avg_cs = mean(Critic_Score, na.rm = TRUE))
+
+print (avg_scores)
+print (df_compare)
